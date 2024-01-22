df963b8e90cad81bf7428fa2d0781c506aa5ba8e
ikilinc1 <ikilinc1@binghamton.edu>
2024-01-22 13:35:36 -0500

    Throwing axes done

--- -Unity-Bullet-Hell/Assets/Scripts/EnemyController.cs
+++ -Unity-Bullet-Hell/Assets/Scripts/EnemyController.cs
@@ -26,24 +26,31 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (knockBackCounter > 0)
+        if (PlayerController.instance.gameObject.activeSelf)
         {
-            knockBackCounter -= Time.deltaTime;
-            if (moveSpeed > 0)
+            if (knockBackCounter > 0)
             {
-                moveSpeed = -moveSpeed * 2f;
+                knockBackCounter -= Time.deltaTime;
+                if (moveSpeed > 0)
+                {
+                    moveSpeed = -moveSpeed * 2f;
+                }
+                if (knockBackCounter <=0)
+                {
+                    moveSpeed = Math.Abs(moveSpeed * 0.5f);
+                }
             }
-            if (knockBackCounter <=0)
+        
+            theRB.velocity = (target.position - transform.position).normalized * moveSpeed;
+
+            if (hitCounter > 0f)
             {
-                moveSpeed = Math.Abs(moveSpeed * 0.5f);
+                hitCounter -= Time.deltaTime;
             }
         }
-        
-        theRB.velocity = (target.position - transform.position).normalized * moveSpeed;
-
-        if (hitCounter > 0f)
+        else
         {
-            hitCounter -= Time.deltaTime;
+            theRB.velocity = Vector2.zero;
         }
     }
 
